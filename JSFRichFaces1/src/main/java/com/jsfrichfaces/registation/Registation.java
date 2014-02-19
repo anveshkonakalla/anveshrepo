17209405071a871433416aafefbb5fe2f3b14e55
Anvesh Konakalla <anvesh.konakalla13@gmail.com>
2014-02-19 17:50:22 +0530

    Update Registation.java

--- JSFRichFaces1/src/main/java/com/jsfrichfaces/registation/Registation.java
+++ JSFRichFaces1/src/main/java/com/jsfrichfaces/registation/Registation.java
@@ -7,8 +7,7 @@ package com.jsfrichfaces.registation;
  */
 public class Registation {
 	
-	private int anvesh1;
-	private int some;
+	
 	private int rid;
 	private String uname;
 	private String fname;

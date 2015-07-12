87545591a41d8830db10a06b91aa4fb7ea1a9dd5
anvesh <anvesh.konakalla13@gmail.com>
2015-07-12 05:08:37 -0700

    method added m()

--- AnveshTest/src/com/anvesh/example/Test2.java
+++ AnveshTest/src/com/anvesh/example/Test2.java
@@ -2,6 +2,9 @@ package com.anvesh.example;
 
 public class Test2 {
 
+	public void m() {
+		System.out.println("method added m()");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("");

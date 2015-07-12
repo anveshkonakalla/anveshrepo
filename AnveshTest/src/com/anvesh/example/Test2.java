0b2f772f176e55a69e6a7f61011c507c9d35a6a3
anvesh <anvesh.konakalla13@gmail.com>
2015-07-12 05:12:44 -0700

    Commmitted for sdfsdf

--- AnveshTest/src/com/anvesh/example/Test2.java
+++ AnveshTest/src/com/anvesh/example/Test2.java
@@ -4,9 +4,10 @@ public class Test2 {
 
 	public void m() {
 		System.out.println("method added m()");
+		System.out.println("*8888888888888888");
 	}
 	public static void main(String[] args) {
-		
+		System.out.println("some");
 		System.out.println("");
 	}
 

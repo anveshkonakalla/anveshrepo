e37a5f658fc2d830470cbb63c14a8bddbc3d5b97
anvesh <anvesh.konakalla13@gmail.com>
2015-07-12 05:15:16 -0700

    Committed

--- AnveshTest/src/com/anvesh/example/Test2.java
+++ AnveshTest/src/com/anvesh/example/Test2.java
@@ -3,13 +3,11 @@ package com.anvesh.example;
 public class Test2 {
 
 	public void m() {
-		System.out.println("method added m()");
-		System.out.println("*8888888888888888");
-		System.out.println("some");
+		System.out.println("m()");
 	}
 	public static void main(String[] args) {
 		
-		System.out.println("");
+		
 	}
 
 }

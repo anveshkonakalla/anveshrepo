e4f389f9f0fbcc2e4701343751f553cbb6eb42de
anvesh <anvesh.konakalla13@gmail.com>
2015-07-12 04:58:31 -0700

    deleted method

--- AnveshTest/src/com/anvesh/example/Test2.java
+++ AnveshTest/src/com/anvesh/example/Test2.java
@@ -2,8 +2,6 @@ package com.anvesh.example;
 
 public class Test2 {
 
-	public void m() {
-	}
 	public static void main(String[] args) {
 		System.out.println("created by anvesh");
 		System.out.println("created by anvesh again");

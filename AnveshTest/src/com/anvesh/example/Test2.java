cb4a7619a5ea87cf846f233c8a6b7044373d0483
anvesh <anvesh.konakalla13@gmail.com>
2015-07-12 04:42:41 -0700

    Committed by Anvesh1

--- AnveshTest/src/com/anvesh/example/Test2.java
+++ AnveshTest/src/com/anvesh/example/Test2.java
@@ -2,6 +2,9 @@ package com.anvesh.example;
 
 public class Test2 {
 
+	public void m() {
+		System.out.println("created by anvesh1");
+	}
 	public static void main(String[] args) {
 		System.out.println("created by anvesh");
 	}

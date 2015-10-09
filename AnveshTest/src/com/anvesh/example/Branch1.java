c1b2e86a46d41183ac4c03f7128af5ef2de1b057
Anvesh.Konakalla <anvesh.konakalla@dealertrack.com>
2015-10-09 16:01:18 +0530

    Comitted

--- AnveshTest/src/com/anvesh/example/Branch1.java
+++ AnveshTest/src/com/anvesh/example/Branch1.java
@@ -2,6 +2,9 @@ package com.anvesh.example;
 
 public class Branch1 {
 
+	public void m1() {
+		System.out.println("m1()");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("main");

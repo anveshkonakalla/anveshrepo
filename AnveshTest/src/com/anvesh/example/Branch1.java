e671f425d62c0d7ee6fcf23f6bc61f271dd6831a
Anvesh.Konakalla <anvesh.konakalla@dealertrack.com>
2015-10-09 15:59:15 +0530

    comitted

--- AnveshTest/src/com/anvesh/example/Branch1.java
+++ AnveshTest/src/com/anvesh/example/Branch1.java
@@ -5,10 +5,7 @@ public class Branch1 {
 	public static void main(String[] args) {
 		
 		System.out.println("main");
-<<<<<<< HEAD
 		System.out.println("git2");
-=======
->>>>>>> branch 'master' of https://github.com/anveshkonakalla/anveshrepo.git
 		System.out.println("git1");
 
 	}

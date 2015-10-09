84526f321e8336ad21149469020f55a2e07a8bf8
Anvesh.Konakalla <anvesh.konakalla@dealertrack.com>
2015-10-09 16:46:05 +0530

    sdfsdf

--- AnveshTest/src/com/anvesh/example/Branch1.java
+++ AnveshTest/src/com/anvesh/example/Branch1.java
@@ -3,14 +3,10 @@ package com.anvesh.example;
 public class Branch1 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-=======
 		System.out.println("some");
 		System.out.println("main");
 		System.out.println("dddddddddddddddd");
 
->>>>>>> refs/heads/GIT2
 	}
 
 }

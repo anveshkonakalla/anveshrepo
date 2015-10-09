c48180691b7cc747480d496b7eaf7aa011446163
Anvesh.Konakalla <anvesh.konakalla@dealertrack.com>
2015-10-09 15:39:38 +0530

    deleted from brnach1 class

--- AnveshTest/src/com/anvesh/example/Branch1.java
+++ AnveshTest/src/com/anvesh/example/Branch1.java
@@ -3,9 +3,8 @@ package com.anvesh.example;
 public class Branch1 {
 
 	public static void main(String[] args) {
-		System.out.println("main()");
-		System.out.println("main1()");
-		System.out.println("main2()");
+		
+		
 
 	}
 
